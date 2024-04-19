ec2ffb9fb3f978772229953b7c7de86e54f09f7a
gvnberaldi <giovanniberaldi43@gmail.com>
2024-04-19 15:46:49 +0200

    CNN model and training added

--- assignments/assignment_1/dlvc/models/cnn.py
+++ assignments/assignment_1/dlvc/models/cnn.py
@@ -1,19 +1,46 @@
-from pathlib import Path
 import torch.nn as nn
-import torch.nn.functional as F
-import torch
-
-
-
 
 class YourCNN(nn.Module):
     def __init__(self):
         super().__init__()
-        ## TODO implement
-        pass
+        self.conv = nn.Sequential(
+            # First Convolutional Layer
+            nn.Conv2d(3, 16, kernel_size=3, padding=1),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            # Second Convolutional Layer
+            nn.Conv2d(16, 32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            # Third Convolutional Layer
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2)
+        )
+
+        self.flatten = nn.Flatten()  # Flatten layer
+
+        self.classifier = nn.Sequential(
+            # First Fully Connected Layer
+            nn.Linear(64 * 4 * 4, 256),
+            nn.ReLU(),
+            # Second Fully Connected Layer
+            nn.Linear(256, 128),
+            nn.ReLU(),
+            # Dropout Layer
+            nn.Dropout(0.5),
+            # Output Layer
+            nn.Linear(128, 10),
+            nn.Softmax(dim=1)  # Softmax activation for class probabilities
+        )
 
     def forward(self, x):
-        ## TODO implement
-        pass
+        x = self.conv(x)
+        x = self.flatten(x)
+        x = self.classifier(x)
+        return x
 
     
---
+++ assignments/assignment_1/train_cnn.py
@@ -0,0 +1,71 @@
+## Feel free to change the imports according to your implementation and needs
+import argparse
+from pathlib import Path
+
+import torch
+import torchvision.transforms.v2 as v2
+from torch.optim import AdamW
+from torch.optim.lr_scheduler import ExponentialLR
+from torchsummary import summary
+
+from dlvc.models.class_model import DeepClassifier  # etc. change to your model
+from dlvc.models.cnn import YourCNN
+from dlvc.metrics import Accuracy
+from dlvc.trainer import ImgClassificationTrainer
+from dlvc.datasets.cifar10 import CIFAR10Dataset
+from dlvc.datasets.dataset import Subset
+
+
+def train(args):
+
+    transform = v2.Compose([v2.ToImage(),
+                            v2.ToDtype(torch.float32, scale=True),
+                            v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+
+    fdir = "data\\cifar-10-batches-py"
+
+    train_data = CIFAR10Dataset(fdir=fdir, subset=Subset.TRAINING, transform=transform)
+    val_data = CIFAR10Dataset(fdir=fdir, subset=Subset.VALIDATION, transform=transform)
+    test_data = CIFAR10Dataset(fdir=fdir, subset=Subset.TEST, transform=transform)
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"Device: {device}")
+
+    model = DeepClassifier(YourCNN())
+    model = model.to(device)
+    summary(model, input_size=(3, 32, 32))
+
+    optimizer = AdamW(model.parameters(), lr=0.001, amsgrad=True)
+    loss_fn = torch.nn.CrossEntropyLoss()
+
+    train_metric = Accuracy(classes=train_data.classes)
+    val_metric = Accuracy(classes=val_data.classes)
+    val_frequency = 5
+
+    model_save_dir = Path("saved_models\\cnn")
+    model_save_dir.mkdir(exist_ok=True)
+
+    lr_scheduler = ExponentialLR(optimizer=optimizer, gamma=0.9)
+
+    trainer = ImgClassificationTrainer(model,
+                                       optimizer,
+                                       loss_fn,
+                                       lr_scheduler,
+                                       train_metric,
+                                       val_metric,
+                                       train_data,
+                                       val_data,
+                                       device,
+                                       args.num_epochs,
+                                       model_save_dir,
+                                       batch_size=128,  # feel free to change
+                                       val_frequency=val_frequency)
+    trainer.train()
+
+if __name__ == "__main__":
+    args = argparse.ArgumentParser(description='Training')
+    if not isinstance(args, tuple):
+        args = args.parse_args()
+
+    args.num_epochs = 30
+    train(args)
